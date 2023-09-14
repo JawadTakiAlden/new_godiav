3853e6a8c666299bda5f30d6fe6ba719844583da
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2023-09-14 03:52:10 -0400

    create user controller and user resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $users = User::all();
+        return UserResource::collection($users);
+    }
+
+    public function store(StoreUserRequest $request){
+        $request->validated($request->all());
+    }
 }
