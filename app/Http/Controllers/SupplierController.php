6a5437b17aa1d92e171abdde05b8fe66a80eafe4
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2023-09-14 04:54:21 -0400

    create supplier CRUD controller

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -2,64 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\CustomResponse\ApiResponse;
+use App\Http\Requests\StoreSupplierRequest;
+use App\Http\Requests\UpdateSupplierRequest;
+use App\Http\Resources\SupplierResource;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    use ApiResponse;
+    public function index(){
+        $suppliers = Supplier::all();
+        return SupplierResource::collection($suppliers);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function store(StoreSupplierRequest $request){
+        $request->validated($request->all());
+        $supplier = Supplier::create($request->all());
+        return SupplierResource::make($supplier);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function update(UpdateSupplierRequest $request , Supplier $supplier){
+        $request->validated($request->all());
+        $supplier->update($request->all());
+        return SupplierResource::make($supplier);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Supplier $supplier)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Supplier $supplier)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Supplier $supplier)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Supplier $supplier)
-    {
-        //
+    public function delete(Supplier $supplier){
+        $supplier->delete();
+        return $this->success($supplier , 'Supplier Deleted Successfully From Our System');
     }
 }
