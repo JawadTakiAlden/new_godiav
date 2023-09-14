de0945c444749a227c7c0c391418b672a238eed3
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2023-09-14 06:51:54 -0400

    fix Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
     protected $guarded = ['id'];
 
     public function ingredients() {
-        return $this->hasMany(ingredients::class);
+        return $this->hasMany(Ingredient::class);
     }
 
     public function category(){
