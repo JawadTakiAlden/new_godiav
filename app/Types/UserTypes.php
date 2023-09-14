61735efcf4e68bc25257d3827fa9bb26e7ad1a0f
JawadTakiAlden <101710481+JawadTakiAlden@users.noreply.github.com>
2023-09-14 03:06:35 -0400

    make ingrediant and user table

---
+++ app/Types/UserTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Types;
+
+class UserTypes
+{
+    const WAITER = 'waiter';
+    const KITCHEN = 'kitchen';
+    const ACCOUNTANT = 'accountant';
+    const ADMIN = 'admin';
+    const SUPPLIER = 'supplier';
+}
